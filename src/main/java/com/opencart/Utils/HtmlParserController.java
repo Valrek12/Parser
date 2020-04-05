e1b4963e3e818a0e36c56e8dc1d1cffdba844f09
Valrek12 <46443310+valrek12@users.noreply.github.com>
2020-04-05 14:46:03 +0300

    refactor Class HtlParserController

--- src/main/java/com/opencart/Utils/HtmlParserController.java
+++ src/main/java/com/opencart/Utils/HtmlParserController.java
@@ -9,36 +9,53 @@ import java.util.ArrayList;
 
 public class HtmlParserController {
 
-    public ArrayList<String> Complects;
-    public ArrayList<String> Specifications;
+    private ArrayList<String> Complects;
+    private ArrayList<String> Specifications;
+    private String fragment;
 
 
     @Contract(pure = true)
     public HtmlParserController(String fragment){
       this.Complects = new ArrayList<>();
       this.Specifications = new ArrayList<>();
-      ParsingFragment(fragment);
+      this.fragment = fragment;
     }
 
-
-    private void ParsingFragment(String fragment){
+    /**
+     * Метод получение Комплектаций товара
+     * @return
+     */
+    public ArrayList<String> getComplects(){
         Document document = Jsoup.parseBodyFragment(fragment);
         Elements dc = document.select("p");
-        for(int i=0; i< dc.size(); i++){
-           String tag = dc.get(i).text();
-           if(tag.indexOf("Комплектация:") != -1){
-            String[] stringArray = tag.split("—");
-            for(int k=1; k< stringArray.length; k++){
-                Complects.add(stringArray[k]);
+        for(int i=0; i< dc.size(); i++) {
+            String tag = dc.get(i).text();
+            if (tag.indexOf("Комплектация:") != -1) {
+                String[] stringArray = tag.split("—");
+                for (int k = 1; k < stringArray.length; k++) {
+                    Complects.add(stringArray[k]);
+                }
             }
-           }
-           else if(tag.indexOf("Характеристики:") != -1){
-               String[] stringArray = tag.split("—");
-               for(int k=1; k< stringArray.length; k++){
-                   Specifications.add(stringArray[k]);
-               }
-           }
         }
+        return Complects;
+    }
 
+    /**
+     * Метод получение характеристик
+     * @return Array<List>Specification</List>
+     */
+    public ArrayList<String> getSpecifications(){
+        Document document = Jsoup.parseBodyFragment(fragment);
+        Elements dc = document.select("p");
+        for(int i=0; i< dc.size(); i++) {
+            String tag = dc.get(i).text();
+            if (tag.indexOf("Харакiтеристики:") != -1) {
+                String[] stringArray = tag.split("—");
+                for (int k = 1; k < stringArray.length; k++) {
+                    Specifications.add(stringArray[k]);
+                }
+            }
+        }
+        return Specifications;
     }
 }
