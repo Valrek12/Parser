1850b68a929db9a2d4368d8fb629ad00dc552736
vchernousov <valrek127@gmail.com>
2020-05-06 20:59:05 +0300

    add path to resources

--- src/main/java/com/opencart/webControllers/HandlingFormSubmissionApplication.java
+++ src/main/java/com/opencart/webControllers/HandlingFormSubmissionApplication.java
@@ -2,11 +2,22 @@ package com.opencart.webControllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class HandlingFormSubmissionApplication {
+public class HandlingFormSubmissionApplication implements WebMvcConfigurer {
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/"
+    };
 
     public static void main(String[] args) {
         SpringApplication.run(HandlingFormSubmissionApplication.class, args);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 }
